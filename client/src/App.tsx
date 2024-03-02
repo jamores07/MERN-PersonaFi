13cdfbb5546f1e7d1935d040cf9ed17e519d6fd0
John A <kayak.stated_0b@icloud.com>
2024-03-02 01:20:24 +0000

    Navbar -- In Progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,34 @@
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+/* Scenes */
+import Navbar from "@/scenes/navbar";
+
 
 
 function App() {
-  
+  const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
-    <div className="App">
-
-
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<div> Dashboard page</div>} />
+                <Route path="/predictions" element={<div> Prediction page</div>} />
+              </Routes>
+            </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
-  )
+  
+  );
 }
 
 export default App
