296f24bb75633ac77f52111568e6ce84c19ca943
John A <kayak.stated_0b@icloud.com>
2024-03-06 03:58:23 +0000

    Dashboard Row 2 completed.

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,17 +1,21 @@
 
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { GetKpisResponse } from "./types";
+import { GetKpisResponse, GetProductsResponse } from "./types";
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
     reducerPath: "main",
-    tagTypes: ["KPIs"],
+    tagTypes: ["KPIs", "Products"],
     endpoints: (build) => ({
         getKPIs: build.query<Array<GetKpisResponse>, void>({
             query: () => "kpi/kpis/",
             providesTags: ["KPIs"]
-        })
+        }),
+        getProducts: build.query<Array<GetProductsResponse>, void>({
+            query: () => "product/products/",
+            providesTags: ["Products"],
+          }),
     })
 })
 
-export const { useGetKPIsQuery } = api;
+export const { useGetKPIsQuery, useGetProductsQuery } = api;
--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ import morgan from "morgan";
 
 import KPI from "./models/KPI.js";
 import kpiRoutes from "./routes/kpi.js"
-import { kpis } from "./data/data.js";
+import Product from "./models/Product.js";
+import productRoutes from "./routes/product.js";
+import { kpis, products } from "./data/data.js";
 
 
 /* CONFIGURATIONS */
@@ -24,6 +26,7 @@ app.use(cors());
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
